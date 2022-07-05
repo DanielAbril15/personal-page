bbceec2ec103a2b910c54e69f170023b7f0202cb
Daniel Abril <danbril1015@gmail.com>
2022-07-05 15:10:32 -0500

    Agrego loader y card de el juego ppt

--- index/js/code.js
+++ index/js/code.js
@@ -1,6 +1,14 @@
 const iconBurger = document.querySelector("#icon__burger");
 const menu = document.querySelector("#menu");
 
+window.addEventListener("load", function () {
+  const loader = this.document.querySelector(".loader");
+
+  setTimeout(function () {
+    loader.style.display = "none";
+  }, 3000);
+});
+
 iconBurger.addEventListener("click", function () {
   menu.classList.toggle("show__menu");
 });
